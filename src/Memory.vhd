4dfa3a2a582577b96ccecfefb0b267b6f5574dbc
BYVoid <byvoid@byvoid.com>
2012-11-07 16:27:56 +0800

    Change on names of interfaces

--- src/Memory.vhd
+++ src/Memory.vhd
@@ -12,16 +12,16 @@ entity Memory is
     data:         inout   std_logic_vector (31 downto 0);
     
     -- Hardware --
-    data_1:       inout   std_logic_vector (15 downto 0);
-    addr_1:       out     std_logic_vector (17 downto 0);
-    oe_1:         out     std_logic;
-    we_1:         out     std_logic;
-    en_1:         out     std_logic;
-    data_2:       inout   std_logic_vector (15 downto 0);
-    addr_2:       out     std_logic_vector (17 downto 0);
-    oe_2:         out     std_logic;
-    we_2:         out     std_logic;
-    en_2:         out     std_logic
+    data1:        inout   std_logic_vector (15 downto 0);
+    addr1:        out     std_logic_vector (17 downto 0);
+    oe1:          out     std_logic;
+    we1:          out     std_logic;
+    en1:          out     std_logic;
+    data2:        inout   std_logic_vector (15 downto 0);
+    addr2:        out     std_logic_vector (17 downto 0);
+    oe2:          out     std_logic;
+    we2:          out     std_logic;
+    en2:          out     std_logic
   );
 end Memory; 
 
