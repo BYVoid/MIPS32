aa1571288efc7cebb9e732621751201581f7539a
BYVoid <byvoid@byvoid.com>
2012-11-12 15:07:59 +0800

    Implement ALU

--- src/ALU.vhd
+++ src/ALU.vhd
@@ -1,42 +1,71 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
+use work.Common.all;
 
 entity ALU is
   port (
     -- Interface --
-    operator:     in      std_logic_vector (3 downto 0);
-    operand1:     in      std_logic_vector (31 downto 0);
-    operand2:     in      std_logic_vector (31 downto 0);
-    result:       out     std_logic_vector (31 downto 0)
+    operator:     in      std_logic_vector(4 downto 0);
+    operand1:     in      std_logic_vector(31 downto 0);
+    operand2:     in      std_logic_vector(31 downto 0);
+    result:       out     std_logic_vector(31 downto 0)
   );
 end ALU;
 
 architecture Behavioral of ALU is
-  signal add_r, sub_r, sll_r, sla_r, rol_r : std_logic_vector (31 downto 0);  
 begin
-
   process(operand1, operand2)
-    variable sll_tmp: std_logic_vector (31 downto 0);
+    variable op1_s, op2_s: Signed32;
+    variable op1_us, op2_us: Unsigned32;
   begin
-    sll_tmp := std_logic_vector(unsigned(operand1) sll to_integer(unsigned(operand2)));
-    sll_r <= sll_tmp;	 
-    sla_r <= operand1(31) & sll_tmp(30 downto 0);
-    rol_r <= std_logic_vector(unsigned(operand1) rol to_integer(unsigned(operand2)));
-    add_r <= std_logic_vector(unsigned(operand1) + unsigned(operand2));
-    sub_r <= std_logic_vector(unsigned(operand1) - unsigned(operand2));
+    op1_s := signed(operand1);
+    op2_s := signed(operand2);
+    op1_us := unsigned(operand1);
+    op2_us := unsigned(operand2);
+    case operator is
+      when "00000" =>
+        result <= operand1 and operand2;
+      when "00001" =>
+        result <= operand1 or operand2;
+      when "00010" =>
+        result <= operand1 xor operand2;
+      when "00011" =>
+        result <= operand1 nor operand2;
+      when "00100" =>
+        result <= not operand1;
+      when "00101" => -- plus
+        result <= std_logic_vector(op1_us + op2_us);
+      when "00110" => -- minus
+        result <= std_logic_vector(op1_us - op2_us);
+      when "00111" => -- shift right logic
+        result <= std_logic_vector(shift_right(op1_us, to_integer(op2_us)));
+      when "01000" => -- shift right arithmetic
+        result <= std_logic_vector(shift_right(op1_s, to_integer(op2_us)));
+      when "01001" => -- shift left logic
+        result <= std_logic_vector(shift_left(op1_us, to_integer(op2_us)));
+      when "01010" => -- equal
+        result <= Int31_Zero & boolean_to_std_logic(operand1 = operand2);
+      when "01011" => -- not equal
+        result <= Int31_Zero & boolean_to_std_logic(operand1 /= operand2);
+      when "01100" => -- less than (unsigned)
+        result <= Int31_Zero & boolean_to_std_logic(op1_us < op2_us);
+      when "01101" => -- less than (signed)
+        result <= Int31_Zero & boolean_to_std_logic(op1_s < op2_s);
+      when "01110" => -- greater than (unsigned)
+        result <= Int31_Zero & boolean_to_std_logic(op1_us > op2_us);
+      when "01111" => -- greater than (signed)
+        result <= Int31_Zero & boolean_to_std_logic(op1_s > op2_s);
+      when "10000" => -- less than or equal (unsigned)
+        result <= Int31_Zero & boolean_to_std_logic(op1_us <= op2_us);
+      when "10001" => -- less than or equal (signed)
+        result <= Int31_Zero & boolean_to_std_logic(op1_s <= op2_s);
+      when "10010" => -- greater than or equal (unsigned)
+        result <= Int31_Zero & boolean_to_std_logic(op1_us >= op2_us);
+      when "10011" => -- greater than or equal (signed)
+        result <= Int31_Zero & boolean_to_std_logic(op1_s >= op2_s);
+      when others =>
+        result <= Int32_Zero;
+    end case;
   end process;
-
-  with operator select
-    r <= add_r when "0000",
-    sub_r when "0001",
-    a and b when "0010",
-    a or b when "0011",
-    a xor b when "0100",
-    not a when "0101",
-    sll_r when "0110",
-    sla_r when "0111",
-    rol_r when "1000",
-    "----------------" when others;
-
 end Behavioral;
